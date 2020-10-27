813a97296128350f14e3265d0ca8cddea38c382c
rdb112 <drrdb112@gmail.com>
2020-10-27 17:08:04 +1300

    First commit...

---
+++ src/App.java
@@ -0,0 +1,28 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte myByte = 5;			// byte
+		short myShort = 5000;		// short
+		int myInt = 65000;			// integer
+		long myLong = 1000000;		// long
+		float myFloat = 10.034f;		// float
+		double myDouble = 3.14159;		// double
+		boolean myBoolean = true;		// boolean
+		char myChar = 'm';
+		
+		System.out.println(myByte);
+		System.out.println(myShort);
+		System.out.println(myInt);
+		System.out.println(myLong);
+		System.out.println(myFloat);
+		System.out.println(myDouble);
+		System.out.println(myBoolean);
+		System.out.println(myChar);
+		
+		
+	}
+
+}
